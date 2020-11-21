7315d7bce7991203ae01dd3d8799462e92a2e801
Mert Can Bilgiç <mert@ieuacm.com>
2020-11-21 17:15:37 +0300

    select theme updated

--- frontend/components/theme-select/index.js
+++ frontend/components/theme-select/index.js
@@ -5,29 +5,40 @@ import styles from './style.module.css'
 import StoreContext from '../../store'
 
 const THEME = {
-  light: 'Light',
-  dim: 'Dim',
-  dark: 'Dark'
+	light: 'Light',
+	dim: 'Dim',
+	dark: 'Dark'
 }
 
-function ThemeSelect({}) {
-  const store = useContext(StoreContext)
+function ThemeSelect({ }) {
+	const store = useContext(StoreContext)
 
-  return (
-    <div className={styles.container}>
-      {['light', 'dim', 'dark'].map((theme) => (
-        <label key={theme} className={styles.label}>
-          <input
-            type="radio"
-            value={theme}
-            name="theme"
-            checked={theme === store.theme}
-            onChange={(e) => store.changeTheme(e.target.value)}
-          />
-          {THEME[theme]}
-        </label>
-      ))}
-    </div>
-  )
+	return (
+
+		<div className={styles.container}>
+			<h2 className={styles.h2}>Select theme</h2>
+			<br />
+			<br />
+			<hr  style={{
+				color: '#fff',
+				backgroundColor: '#fff',
+				height: .5,
+				borderColor: '#fff'
+			}} />
+			<br />
+			{['light', 'dim', 'dark'].map((theme) => (
+				<label key={theme} className={styles.label}>
+					<input
+						type="radio"
+						value={theme}
+						name="theme"
+						checked={theme === store.theme}
+						onChange={(e) => store.changeTheme(e.target.value)}
+					/>
+					{THEME[theme]}
+				</label>
+			))}
+		</div>
+	)
 }
 export default ThemeSelect
